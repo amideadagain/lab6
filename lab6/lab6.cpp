ab9a9303ae8ebea896c383a5d09522c7f0a25bf6
kote6725@gmail.com <kote6725@gmail.com>
2019-06-07 00:46:43 +0300

    Добавьте файлы проекта.

---
+++ lab6/lab6.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+
+	struct Autor
+	{
+		std::string name;
+		std::string book;
+		size_t count;
+		size_t year;
+		void show()
+		{
+			std::cout << "============================\n";
+			std::cout << "Name :" << name << std::endl;
+			std::cout << "Book :" << book << std::endl;
+			std::cout << "Count :" << count << std::endl;
+			std::cout << "Year :" << year << std::endl;
+		}
+	};
+
+	std::ifstream bin_file;
+	std::string buff;
+	bin_file.open("D:\sss.txt");
+	while (!bin_file.eof())
+	{
+		Autor temp;
+		std::getline(bin_file, temp.name);
+		std::getline(bin_file, temp.book);
+		bin_file >> temp.count;
+		bin_file >> temp.year;
+		if (temp.book.find("dead") != std::string::npos)
+		{
+			temp.show();
+		}
+		
+	}
+}
